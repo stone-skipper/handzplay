52371738c47c84d3f4323287fbbfa0f1a28e283b
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-07-28 00:13:40 +0800

    updating control

--- components/UI/controls/divider.js
+++ components/UI/controls/divider.js
@@ -1,3 +1,22 @@
-export default function Divider({ color }) {
-  return <div style={{ height: 1, width: "100%", background: color }} />;
+export default function Divider({ color, direction, customSize = null }) {
+  if (direction === "horizontal")
+    return (
+      <div
+        style={{
+          height: 1,
+          width: customSize === null ? "100%" : customSize,
+          background: color,
+        }}
+      />
+    );
+  else if (direction === "vertical")
+    return (
+      <div
+        style={{
+          height: customSize === null ? "100%" : customSize,
+          width: 1,
+          background: color,
+        }}
+      />
+    );
 }
--- components/controlUI/controls.js
+++ components/controlUI/controls.js
@@ -3,6 +3,9 @@ import styles from "./controls.module.scss";
 import { useEffect, useState } from "react";
 import ColorSelect from "../UI/controls/color";
 import Divider from "../UI/controls/divider";
+import HandIllust01 from "../visual/hand01";
+import HandIllust02 from "../visual/hand02";
+import { motion } from "framer-motion";
 
 export default function Controls() {
   const cameraFeed = useControlsStore((state) => state.cameraFeed);
@@ -22,47 +25,148 @@ export default function Controls() {
 
   return (
     <div className={styles.wrapper}>
-      <div
+      <motion.div
         className={styles.content}
         style={{ display: controlToggle === true ? "flex" : "none" }}
       >
         <div className={styles.statusBox}>
           <div>
-            illustrations
-            <p>{handReady === true ? "active" : "loading..."}</p>
+            <motion.div
+              style={{
+                position: "relative",
+                scale: 0.4,
+                height: 100,
+                width: "90%",
+              }}
+            >
+              <HandIllust01
+                initTop={-150}
+                initLeft={-320}
+                top={-150}
+                left={-320}
+              />
+              <HandIllust02
+                initTop={-130}
+                initLeft={140}
+                top={-130}
+                left={140}
+              />
+            </motion.div>
+            <div>
+              <span className={styles.status}>
+                {handReady === true ? "Active" : "Loading..."}
+              </span>
+              <br />
+              <span className={styles.title}>Hand Recognition</span>
+            </div>
           </div>
 
-          <div style={{ display: "flex" }}>
-            <p style={{ width: "50%", opacity: leftHand === true ? 1 : 0.4 }}>
-              left: {currentPoseL}
-            </p>
-            <p style={{ width: "50%", opacity: leftHand === true ? 1 : 0.4 }}>
-              right: {currentPoseR}
-            </p>
+          <div
+            style={{
+              display: "flex",
+              borderTop: "1px solid white",
+              width: "90%",
+              alignItems: "center",
+              paddingTop: 6,
+            }}
+          >
+            <div
+              style={{
+                width: "50%",
+                opacity: leftHand === true ? 1 : 0.4,
+                height: "fit-content",
+              }}
+            >
+              <span className={styles.status}>
+                {currentPoseL !== "" ? currentPoseL : "--"}
+              </span>
+              <br /> <span className={styles.title}>left</span>
+            </div>
+            <Divider direction="vertical" color="white" customSize={25} />
+            <div
+              style={{
+                width: "50%",
+                opacity: rightHand === true ? 1 : 0.4,
+                height: "fit-content",
+              }}
+            >
+              <span className={styles.status}>
+                {currentPoseR !== "" ? currentPoseR : "--"}
+              </span>
+              <br /> <span className={styles.title}>right</span>
+            </div>
           </div>
         </div>
 
-        <Divider color="lightgrey" />
-        <p
-          onClick={() => {
-            useControlsStore.setState({ cameraFeed: !cameraFeed });
-          }}
-        >
-          camera Feed: {cameraFeed.toString()}
-        </p>
-        <p
-          onClick={() => {
-            if (handIndicatorType === "skeleton") {
-              useControlsStore.setState({ handIndicatorType: "points" });
-            } else if (handIndicatorType === "points") {
-              useControlsStore.setState({ handIndicatorType: "blurred" });
-            } else if (handIndicatorType === "blurred") {
-              useControlsStore.setState({ handIndicatorType: "skeleton" });
-            }
-          }}
-        >
-          hand type: {handIndicatorType}
-        </p>
+        <div className={styles.controlWrapper}>
+          <div className={styles.controlTitle}>Camera Feed</div>
+          <div className={styles.options}>
+            <div
+              onClick={() => {
+                useControlsStore.setState({ cameraFeed: true });
+              }}
+              style={{
+                color: cameraFeed === true ? "#004DC0" : "grey",
+              }}
+            >
+              on
+            </div>
+            <div className={styles.dividerPosition}>
+              <Divider direction="vertical" customSize={7} color="grey" />
+            </div>
+            <div
+              onClick={() => {
+                useControlsStore.setState({ cameraFeed: false });
+              }}
+              style={{
+                color: cameraFeed === false ? "#004DC0" : "grey",
+              }}
+            >
+              off
+            </div>
+          </div>
+        </div>
+        <div className={styles.controlWrapper}>
+          <div className={styles.controlTitle}>Hand Type</div>
+          <div className={styles.options}>
+            <div
+              onClick={() => {
+                useControlsStore.setState({ handIndicatorType: "points" });
+              }}
+              style={{
+                color: handIndicatorType === "points" ? "#004DC0" : "grey",
+              }}
+            >
+              point
+            </div>
+            <div className={styles.dividerPosition}>
+              <Divider direction="vertical" customSize={7} color="grey" />
+            </div>
+            <div
+              onClick={() => {
+                useControlsStore.setState({ handIndicatorType: "skeleton" });
+              }}
+              style={{
+                color: handIndicatorType === "skeleton" ? "#004DC0" : "grey",
+              }}
+            >
+              skeleton
+            </div>
+            <div className={styles.dividerPosition}>
+              <Divider direction="vertical" customSize={7} color="grey" />
+            </div>
+            <div
+              onClick={() => {
+                useControlsStore.setState({ handIndicatorType: "blurred" });
+              }}
+              style={{
+                color: handIndicatorType === "blurred" ? "#004DC0" : "grey",
+              }}
+            >
+              shadow
+            </div>
+          </div>
+        </div>
 
         <p style={{ display: "flex", alignItems: "center" }}>
           hand color <ColorSelect />
@@ -70,7 +174,7 @@ export default function Controls() {
         <p style={{ display: "flex", alignItems: "center" }}>
           playground color <ColorSelect />
         </p>
-      </div>
+      </motion.div>
       <div
         className={styles.header}
         onClick={() => {
