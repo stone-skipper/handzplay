8ab2b63ad0c042fc8ace8fdb6072385d00cac251
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-10-09 21:49:07 +0800

    add mobile

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
 import Handpose from "../components/handpose";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../components/UI/logo";
 import Footer from "../components/UI/footer";
 import { useControlsStore } from "../lib/store";
@@ -12,55 +12,29 @@ import LoadingSlider from "../components/UI/loadingSlider";
 import { motion } from "framer-motion";
 import HandIllust03 from "../components/visual/hand03";
 import HandIllust04 from "../components/visual/hand04";
+import { useViewport } from "../lib/hook";
 
 export default function Index() {
   const leftHand = useControlsStore((state) => state.leftHand);
   const rightHand = useControlsStore((state) => state.rightHand);
   const currentPoseL = useControlsStore((state) => state.currentPoseL);
   const currentPoseR = useControlsStore((state) => state.currentPoseR);
-  const handReady = useControlsStore((state) => state.handReady);
-  const cameraAccess = useControlsStore((state) => state.cameraAccess);
 
-  const landingRules = [
-    // {
-    //   ifType: "pose",
-    //   pose: ["victory", "left"], //type of gesture, left right both
-    //   thenType: "interface",
-    //   thenDetail: [
-    //     "rect",
-    //     {
-    //       color: "blue",
-    //       opacity: 1,
-    //       width: 100,
-    //       height: 100,
-    //       radius: 10,
-    //       rotate: 0,
-    //     }, // initial
-    //     {
-    //       color: "white",
-    //       opacity: 1,
-    //       width: 150,
-    //       height: 100,
-    //       radius: 10,
-    //       rotate: 45,
-    //       move: ["y", 20], // 'x' or 'y' as axis / movedistance
-    //     }, //onPose
-    //   ],
-    // },
-  ];
-  // const cameraFeed = useControlsStore((state) => state.cameraFeed);
+  const [isMobile, setIsMobile] = useState(false);
+  const breakpoint = 640;
+  const { width } = useViewport();
+
   useEffect(() => {
     if (currentPoseL === "thumbs_up" && currentPoseR === "thumbs_up") {
       window.location.href = "/playground";
     }
   }, [currentPoseL, currentPoseR]);
 
-  return (
+  return width > breakpoint ? (
     <div className={styles.app}>
       <Handpose
         handIndicatorType={"blurred"}
         cameraFeed={false}
-        rules={landingRules}
         handColor="white"
       />
 
@@ -85,6 +59,65 @@ export default function Index() {
       </div>
 
       <Footer />
+    </div>
+  ) : (
+    <div className={styles.mobile}>
+      <motion.div
+        animate={{
+          opacity: rightHand === true || leftHand === true ? 0 : 1,
+          filter:
+            rightHand === true || leftHand === true
+              ? "blur(30px)"
+              : "blur(0px)",
+        }}
+        style={{ width: "100%", height: "100%", position: "absolute", top: 0 }}
+      >
+        <HandIllust01 initTop="20%" initLeft="-50%" top="18%" left="-45%" />
+        <HandIllust02 initTop="35%" initLeft="45%" top="33%" left="40%" />
+        <HandIllust03 initTop="46%" initLeft="-20%" top="45%" left="-15%" />
+        <HandIllust04 initTop="9%" initLeft="45%" top="6%" left="40%" />
+      </motion.div>
+      <div className={styles.titleWrapper}>
+        <Logo color="white" displayTag={false} fontSize={50} delay={3} />
+        <div
+          style={{
+            color: "white",
+            fontSize: "0.7em",
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            textAlign: "center",
+            lineHeight: 1.5,
+          }}
+        >
+          <p>
+            playground for your hands,
+            <br /> instead of your mouse
+          </p>
+        </div>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "15%",
+          color: "white",
+          fontSize: "0.55em",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          textAlign: "center",
+          lineHeight: 1.5,
+        }}
+      >
+        <p>
+          it's not supported on mobile.
+          <br />
+          <br />
+          please visit again
+          <br />
+          when you're on your laptop.
+        </p>
+      </div>
     </div>
   );
 }
